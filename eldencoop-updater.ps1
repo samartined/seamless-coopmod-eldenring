69c4eca3e3cb33c9c9b8f5ab8c06201ca1600d05
Sergio <yupipi93@gmail.com>
2024-07-19 23:26:28 +0200

    [MD] Remove config file

--- eldencoop-updater.ps1
+++ eldencoop-updater.ps1
@@ -11,15 +11,79 @@ function main {
     }
     Write-Debug "Server password set!"
 
-    # Define the game path
-    $global:game_path = "D:\SteamLibrary\steamapps\common\ELDEN RING\Game"
-    Write-Debug "Game path set to $game_path"
+    # Show form to select game path
+    $global:game_path = Show-Form
+    if ($global:game_path) {
+        Write-Debug "Game path set to $global:game_path"
 
-    # Run the update version function with the password and game path parameters
-    update_version -serverPassword $config.ServerPassword -gamePath $game_path
+        # Run the update version function with the password and game path parameters
+        update_version -serverPassword $config.ServerPassword -gamePath $global:game_path
 
-    # Run the game execution function
-    ejecute_game -gamePath $game_path
+        # Run the game execution function
+        ejecute_game -gamePath $global:game_path
+    } else {
+        Write-Output "Game path selection was canceled or no path was entered."
+    }
+}
+
+function Show-Form {
+    # Load Windows Forms assembly for message boxes and form components
+    Add-Type -AssemblyName System.Windows.Forms
+
+    # Create and configure the form
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = "Select Game Path"
+    $form.Size = New-Object System.Drawing.Size(400, 200)
+
+    # Create and configure the label
+    $label = New-Object System.Windows.Forms.Label
+    $label.Text = "Game Path:"
+    $label.Size = New-Object System.Drawing.Size(350, 20)
+    $label.Location = New-Object System.Drawing.Point(20, 20)
+    $form.Controls.Add($label)
+
+    # Create and configure the text box with default value
+    $textbox = New-Object System.Windows.Forms.TextBox
+    $textbox.Text = "C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Game"
+    $textbox.Size = New-Object System.Drawing.Size(350, 20)
+    $textbox.Location = New-Object System.Drawing.Point(20, 50)
+    $form.Controls.Add($textbox)
+
+    # Create and configure the browse button
+    $button = New-Object System.Windows.Forms.Button
+    $button.Text = "Browse"
+    $button.Size = New-Object System.Drawing.Size(75, 23)
+    $button.Location = New-Object System.Drawing.Point(20, 80)
+    $form.Controls.Add($button)
+
+    # Add a folder browser dialog
+    $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+
+    # Add an event handler for the button click event
+    $button.Add_Click({
+        if ($folderBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+            $textbox.Text = $folderBrowser.SelectedPath
+        }
+    })
+
+    # Create and configure the OK button
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Text = "OK"
+    $okButton.Size = New-Object System.Drawing.Size(75, 23)
+    $okButton.Location = New-Object System.Drawing.Point(295, 80)
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.Controls.Add($okButton)
+
+    # Show the form
+    $form.AcceptButton = $okButton
+    $form.StartPosition = "CenterScreen"
+    $result = $form.ShowDialog()
+
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK -and $textbox.Text -ne "") {
+        return $textbox.Text
+    } else {
+        return $null
+    }
 }
 
 function Get-ReleaseInfo {
