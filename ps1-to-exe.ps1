fec621c7edd1573d934693a19e5deb75a93810c1
Sergio <yupipi93@gmail.com>
2024-08-01 21:29:56 +0200

    [WIP] hidden terminal

--- ps1-to-exe.ps1
+++ ps1-to-exe.ps1
@@ -13,27 +13,7 @@ foreach ($module in $modules) {
 $scriptPath = "eldencoop-updater.ps1"
 $outputExePath = "eldencoop-updater.exe"
 
-# Configuration values previously in config.psd1
-$config = @{
-    ServerPassword = "your_password_here"  # Replace with your actual password
-}
-
-# Read the original script content
-$scriptContent = Get-Content -Path $scriptPath -Raw
-
-# Embed the configuration values into the script
-$configPlaceholder = '$config = @{}'
-$configEmbedded = "`$config = @{`n    ServerPassword = `"your_password_here`"  # Replace with your actual password`n}"
-$updatedScriptContent = $scriptContent -replace [regex]::Escape($configPlaceholder), $configEmbedded
-
-# Save the updated script to a temporary file
-$tempUpdatedScriptPath = [System.IO.Path]::GetTempFileName()
-Set-Content -Path $tempUpdatedScriptPath -Value $updatedScriptContent
-
-# Convert the updated script to an executable using PS2EXE
-Invoke-Expression -Command "Invoke-ps2exe $tempUpdatedScriptPath $outputExePath"
-
-# Clean up the temporary file
-Remove-Item -Path $tempUpdatedScriptPath
+# Convert the updated script to an executable using PS2EXE with no console
+Invoke-Expression -Command "Invoke-ps2exe -noConsole $scriptPath $outputExePath"
 
 Write-Host "Conversion complete. The executable is saved as $outputExePath"
